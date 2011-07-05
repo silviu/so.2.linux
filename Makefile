ff54e7559e5eaa55e7053b7b81e3c531bef0d86f
Silviu Grijincu <silviu.grijincu@gmail.com>
2011-07-05 17:12:05 +0300

    Gateway now receives the command in the message queue

--- Makefile
+++ Makefile
@@ -4,10 +4,18 @@ CFLAGS = -Wall -g -lrt
 all:clean server gateway
 
 server: server.c
+	@echo "Compiling sources..."
 	$(CC) $(CFLAGS) server.c -o server
 
 gateway: gateway.c
 	$(CC) $(CFLAGS) gateway.c -o gateway
+	@echo "------------------------------"
+
+test: clean server gateway
+	cd checker; make -f Makefile.checker
+	@echo "------------------------------"
 
 clean:
+	@echo "Cleaning workspace..."
 	rm -rf *.o *~ gateway server
+	@echo "------------------------------"
--- gateway.c
+++ gateway.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <semaphore.h>
@@ -6,24 +7,53 @@
 
 #define MQ_NAME "/gateway"
 
+typedef struct command {
+    char name[8];
+    int dim;
+    short crypt[17];
+} Command, *PCommand;
+
 mqd_t open_msg_queue()
 {
-    //unlink the message queue to be sure it is created
+    // unlink the message queue to be sure it is created
     mq_unlink(MQ_NAME);
+    
+    // initialize mq_attr stucture
+	struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 100;
+    attr.mq_msgsize = 49;
+    attr.mq_curmsgs = 0;
 
-    mqd_t ret = mq_open(MQ_NAME, O_CREAT | O_RDONLY, 0666, NULL);
+    
+    // open message queue
+    mqd_t ret = mq_open(MQ_NAME, O_CREAT | O_RDONLY, 0666, &attr);
     if (ret == (mqd_t)-1) {
-        fprintf(stderr, "Error on mq_open() in open_msg_queue()\n");
-        perror("bls");
+        perror("mq_open");
         return (mqd_t)-1;
     }
-
     return ret;
 }
+
+PCommand get_message(mqd_t mq)
+{
+    unsigned int prio = 10;
+    char* buff = (char*) malloc(100);
+    mq_receive(mq, buff, 100, &prio);
+    return (PCommand)buff;
+}
+
 
 int main(int argc, char** argv)
 {
     printf("Gateway is up.\n");
-    open_msg_queue();
+    
+    mqd_t mq = open_msg_queue();
+    if (mq == (mqd_t)-1)
+        exit(EXIT_FAILURE);
+    PCommand c = get_message(mq);
+    printf("Name = %s\nDim=%d\n", c->name, c->dim);
+    
+	
     return 0;
 }
